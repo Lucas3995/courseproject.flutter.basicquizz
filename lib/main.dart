979d130eebc88333ba620ed1ace304d15635540b
lucas3995 <lucas3995@hotmail.com>
2019-12-24 01:23:20 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+    Widget build(BuildContext context){
+        return MaterialApp(
+            home: Text('teste')
+        );
+    }
+}
