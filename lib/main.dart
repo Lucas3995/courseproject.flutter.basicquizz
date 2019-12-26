48ee03a744e6f6c982d2b52d7b7e792e4e26bd06
lucas3995 <lucas3995@hotmail.com>
2019-12-25 23:07:26 -0300

    Transforming main into a statefull widget and begin app specific code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(MyApp());
+void main() => runApp(QuizzApp());
+
+class QuizzApp extends StatefulWidget {
+  @override
+  _QuizzAppState createState() => _QuizzAppState();
 }
 
-class MyApp extends StatelessWidget{
-    Widget build(BuildContext context){
-        return MaterialApp(
-            home: Text('teste')
-        );
-    }
+class _QuizzAppState extends State<QuizzApp> {
+  var questionIndex = 0;
+
+  void answerQuestion(answer) {
+    setState(() {
+      //incomplete. can be setted to a larger value than the questions count
+      questionIndex++;
+    });
+    print("questionIndex $questionIndex");
+    print("Answer $answer");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?'
+    ];
+
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('Test')),
+      body: Column(
+        children: <Widget>[
+          Text(questions[questionIndex]),
+          RaisedButton(
+              child: Text("Answer1"), onPressed: () => answerQuestion(1)),
+          RaisedButton(
+              child: Text("Answer2"), onPressed: () => answerQuestion(2)),
+          RaisedButton(
+              child: Text("Answer3"), onPressed: () => answerQuestion(3))
+        ],
+      ),
+    ));
+  }
 }
